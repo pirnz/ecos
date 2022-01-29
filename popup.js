3cf45381e3346d0770b3b5fa13a94af2e878c5f5
I334178 <pedro.iranzo.egea@sap.com>
2022-01-29 15:03:04 +0100

    included new scripts

--- popup.js
+++ popup.js
@@ -4,11 +4,15 @@ async function main() {
 
   chrome.scripting.executeScript({
     target: {tabId: tab.id},
-    files: ['lib/jquery.min.js', 'lib/date-scrape.js', 'lib/title-scrape.js', 'scrape.js']}, 
+    files: ['lib/jquery.min.js', 'lib/date-scrape.js', 'lib/site-scrape.js', 'lib/description-scrape.js', 'lib/language-scrape.js', 'lib/keywords-scrape.js', 'lib/title-scrape.js', 'scrape.js']}, 
     function(result){
       //console.error(JSON.stringify(result))
       document.getElementById('title').innerHTML = result[0].result.title;
       document.getElementById('date').innerHTML = result[0].result.date;
+      document.getElementById('language').innerHTML = result[0].result.language;
+      document.getElementById('site').innerHTML = result[0].result.site;
+      document.getElementById('keywords').innerHTML = result[0].result.keywords;
+      document.getElementById('description').innerHTML = result[0].result.description;
     }
   );
 }

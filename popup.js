29652cfb53f1a16aced6fe98a1c90c7a56e075cf
I334178 <pedro.iranzo.egea@sap.com>
2022-02-18 21:24:27 +0100

    added keyword finder

--- popup.js
+++ popup.js
@@ -4,16 +4,20 @@ async function main() {
 
   chrome.scripting.executeScript({
     target: {tabId: tab.id},
-    files: ['lib/jquery.min.js', 'lib/date-scrape.js', 'lib/site-scrape.js', 'lib/description-scrape.js', 'lib/language-scrape.js', 'lib/keywords-scrape.js', 'lib/title-scrape.js', 'lib/data-prep.js']}, 
-    function(result){
-      //console.error(JSON.stringify(result))
-      document.getElementById('title').innerHTML = result[0].result.title;
-      document.getElementById('date').innerHTML = result[0].result.date;
-      document.getElementById('language').innerHTML = result[0].result.language;
-      document.getElementById('site').innerHTML = result[0].result.site;
-      document.getElementById('keywords').innerHTML = result[0].result.keywords;
-      document.getElementById('description').innerHTML = result[0].result.description;
-    }
+    files: ['lib/jquery.min.js', 'lib/date-scrape.js', 'lib/site-scrape.js', 'lib/description-scrape.js', 'lib/language-scrape.js', 'lib/keywords-scrape.js', 'lib/title-scrape.js', 'lib/data-prep-dist.js']}, 
+    function(){
+      chrome.storage.local.get({
+        result: ''
+      }, function(item) {
+        console.error(item.result);
+        document.getElementById('title').innerHTML = item.result.title;
+        document.getElementById('date').innerHTML = item.result.date;
+        document.getElementById('language').innerHTML = item.result.language;
+        document.getElementById('site').innerHTML = item.result.site;
+        document.getElementById('keywords').innerHTML = item.result.keywords;
+        document.getElementById('description').innerHTML = item.result.description;
+    });
+  }
   );
 }
 
